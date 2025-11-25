9211ac40b1f8248546570a9a5cfdb89f83b89862
ahernandezc <alvaro.hernandez@laguaca.cr>
2025-11-24 18:55:25 -0600

    cambios

--- html/header.php
+++ html/header.php
@@ -16,7 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta
       name="keywords"
-      content="control filas la guaca"
+      content="control de filas la guaca"
     />
     <meta
       name="description"
